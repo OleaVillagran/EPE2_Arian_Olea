a62e7ccf45cb4a9c5ffffb557f64b4b27b4c6af1
Arian Olea <arian.olea1717@gmail.com>
2023-10-18 19:52:10 -0300

    Clases Vacias

---
+++ src/main/java/com/mycompany/epe2_arian_olea/EPE2_Arian_Olea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.epe2_arian_olea;
+
+/**
+ *
+ * @author kripp
+ */
+public class EPE2_Arian_Olea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
